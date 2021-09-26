22224b082495c3c4460ff72ef1c01f9ac461f899
Barış Yıldırım <C9010332@arcelik.com>
2021-09-26 15:30:04 +0300

    Setur.Entity içersinde MongoDB bağlantısı kuruldu.

--- Setur.Entity/Models/Person.cs
+++ Setur.Entity/Models/Person.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +11,12 @@ namespace Setur.Entity.Models
 {
     public class Person
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("Name")]
+        [JsonProperty("Name")]
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Company { get; set; }
